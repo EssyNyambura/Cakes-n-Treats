d2a78558e73de3a41cb75a89393af3be1207a328
EssyNyambura <131815539+EssyNyambura@users.noreply.github.com>
2024-07-09 03:17:06 +0300

    Update connectdb.php

---
+++ PHP/connectdb.php
@@ -0,0 +1,19 @@
+<?php
+	# database credentials for the bakery's database
+	$servername = "localhost";
+	$username = "root";
+	$password = "DBPASsword";
+	$dbname = "bakery_orders";
+
+	# Establish a connection 
+	$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+	# Verify connection
+	if($mysqli->connect_error){
+        die("Connection failed!" . $mysqli->connect_error);  // Display the error message
+    } else {
+        echo "Connected successfully";  // Message indicating successful connection
+
+	}
+
+?>
